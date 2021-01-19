59a031c49923ec653ff7a92e8b53e40b82f7b638
Abdurrahman512 <76415954+Abdurrahman512@users.noreply.github.com>
2021-01-19 07:08:21 +0530

    Add files via upload

---
+++ api.py
@@ -0,0 +1,16 @@
+{"comments": 0,
+ "description": null,
+ "files": {"code.py": {"content": "print('some code')",
+                       "filename": "code.py",
+                       "language": "Python",
+                       "raw_url": "https://gist.githubusercontent.com/MartinHeinz/383c6b450f892e169074a642a372e459/raw/8d53df5862f8b687fc09d0b3c1b3c49afe441cbe/code.py",
+                       "size": 18,
+                       "truncated": null,
+                       "type": "application/x-python"}},
+ "forks": [],
+ "html_url": "https://gist.github.com/383c6b450f892e169074a642a372e459",
+ "id": "383c6b450f892e169074a642a372e459",
+ "node_id": "MDQ6R2lzdDM4M2M2YjQ1MGY4OTJlMTY5MDc0YTY0MmEzNzJlNDU5",
+ "public": true,
+ "url": "https://api.github.com/gists/383c6b450f892e169074a642a372e459"
+}
